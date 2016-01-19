46be9ffef1124e33e0aee6bbce5068dc135d9c2b
davidog23 <davidolmos23@gmail.com>
2016-01-19 05:22:18 +0100

    Working extraction with mods which only modifies "gamemode" and "maps".

--- src/net/davidog/gmad/GMAD.java
+++ src/net/davidog/gmad/GMAD.java
@@ -2,11 +2,13 @@ package net.davidog.gmad;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Vector;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
 
 public class GMAD {
+
+    private static File garrysFolder;
 
     public static void main(String[] args) {
 	    Runtime runtime = Runtime.getRuntime();
@@ -16,7 +18,7 @@ public class GMAD {
             File arg = new File(args[0]);
 
             if (arg.isDirectory()) {
-                File garrysFolder = arg.getParentFile();
+                garrysFolder = arg.getParentFile();
 
                 //Descompresión de los .gma
                 List<File> files = Arrays.asList(arg.listFiles(f -> f.isFile()));
@@ -31,14 +33,69 @@ public class GMAD {
 
                 //Mover carpetas
                 List<File> folders = Arrays.asList(arg.listFiles(f -> f.isDirectory())); //folders = ds_XXXXXX
-                Vector<File> archivosAMover = new Vector<>(); //archivosAMover = Cosas dentro de maps y gamemode
-                folders.forEach(t -> Arrays.asList(t.listFiles(f -> f.isDirectory())).forEach(r -> {
-                    if(r.getName().equals("gamemode") || r.getName().equals("maps")) {
-                        archivosAMover.addAll(Arrays.asList(r.listFiles(f -> true))); //Continuar por aqui.
-                    }
-                }));
+                ArrayList<File> archivosAMover = new ArrayList<>(); //archivosAMover = Cosas dentro de maps y gamemode
+                folders.forEach(t -> Arrays.asList(t.listFiles(f -> f.isDirectory())).forEach(r -> archivosAMover.addAll(Arrays.asList(r.listFiles(GMAD::isNotInGarrysFolder)))));
 
+                String s = File.separator;
+                archivosAMover.forEach(f -> {
+                    try {
+                        File target = new File(garrysFolder.getAbsolutePath() + s + f.getParentFile().getName() + s + f.getName());
+                        if(!target.getParentFile().exists()) { target.getParentFile().mkdir(); }
+                        if(f.isFile()) {
+                            Files.copy(f.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                        } else {
+                            copyDir(f, target);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
         }
+    }
+
+    public static boolean isNotInGarrysFolder(File file) {
+        String name = file.getName();
+        ArrayList<File> yaDescomprimido = new ArrayList<>();
+        Arrays.asList(garrysFolder.listFiles(f -> f.getName().equals("gamemodes") || f.getName().equals("maps"))).forEach(f -> yaDescomprimido.addAll(Arrays.asList(f.listFiles())));
+
+        boolean control = true;
+        int n = yaDescomprimido.size();
+        for (int i = 0; i < n && control; i++) {
+            if(name.equals(yaDescomprimido.get(i).getName())){
+                control = false;
+            }
+        }
+        return control;
+    }
+
+    //Sacado de los javadocs de la api...
+    public static void copyDir(File sourceFolder, File targetFolder) throws IOException {
+        Path source = sourceFolder.toPath();
+        Path target = targetFolder.toPath();
+
+        Files.walkFileTree(source, EnumSet.of(FileVisitOption.FOLLOW_LINKS), Integer.MAX_VALUE,
+                new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                            throws IOException
+                    {
+                        Path targetdir = target.resolve(source.relativize(dir));
+                        try {
+                            Files.copy(dir, targetdir);
+                        } catch (FileAlreadyExistsException e) {
+                            if (!Files.isDirectory(targetdir))
+                                throw e;
+                        }
+                        return FileVisitResult.CONTINUE;
+                    }
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                            throws IOException
+                    {
+                        Files.copy(file, target.resolve(source.relativize(file)));
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
     }
 }
