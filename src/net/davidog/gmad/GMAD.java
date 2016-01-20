368e6a3bc0dff4b25890f87fceeac688febf87a1
davidog23 <davidolmos23@gmail.com>
2016-01-20 01:06:16 +0100

    The beginning.

--- src/net/davidog/gmad/GMAD.java
+++ src/net/davidog/gmad/GMAD.java
@@ -6,6 +6,8 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
+
+//https://github.com/garrynewman/gmad
 public class GMAD {
 
     private static File garrysFolder;
